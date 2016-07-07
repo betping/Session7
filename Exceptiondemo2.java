f253023b84cac0b8c4dc688287f1576cdd25ce63
betping <betping1993@gmail.com>
2016-07-08 04:51:47 +0530

    Add files via upload

---
+++ Exceptiondemo2.java
@@ -0,0 +1,14 @@
+
+public class Exceptiondemo2 {
+	
+	   public void main(String args[]){
+	   {
+	      try{
+		 int num=Integer.parseInt ("XYZ") ;
+		 System.out.println(num);
+	      }catch(NumberFormatException e){
+		  System.out.println("Number format exception occurred");
+	       }
+	   }
+	}
+}
